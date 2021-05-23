e0a926020c5d7c8cc1bb5225635291e2cd86f74f
Vibhor <vibhorsxn@gmail.com>
2021-05-23 14:38:34 +0530

    add recipe card

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ const recipe = [
   },
   {
     item: "patties",
-    ingredients: ["refined flour", "onion", "potato"],
+    ingredients: ["tomato", "onion", "capsicum"],
     steps: "*Patties* Mix maida, salt and 1 cup butter into a crumbly consistency.Roll dough into a long strip again, fold one third from one end towards center and then the other. Chill. And repeat twice more before using as required."
   }
 ];
@@ -36,17 +36,43 @@ const checkCondition = (a, b = arr) => {
 };
 
 const handleSubmit = (e) => {
+  document.getElementById("image").style.display = "none";
   removeAllChild(document.getElementById("recipes"));
   for (let i = 0; i < recipe.length; i++) {
     let flag = checkCondition(recipe[i].ingredients);
     if (flag == true) {
       console.log(recipe[i].item);
+
+      // CARD
       let card = document.createElement("div");
       card.classList.add("card");
-      // let para = document.createElement("p");
-      let node = document.createTextNode(recipe[i].steps);
-      // para.classList.add("para");
-      card.appendChild(node);
+
+      // CARD BODY
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      // CARD TITLE
+      let cardTitle = document.createElement("h5");
+      let node = document.createTextNode(recipe[i].item);
+      cardTitle.classList.add("card-title");
+      cardTitle.appendChild(node);
+
+      //CARD img Body
+      let cardImg= document.createElement("img");
+      cardImg.setAttribute("src","./svg/undraw_Hamburger_8ge6.svg");
+      cardImg.classList.add("card-img-top");
+      
+      // CARD TEXT
+      let para = document.createElement("p");
+      node = document.createTextNode(recipe[i].steps);
+      para.classList.add("card-text");
+      para.appendChild(node);
+
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(para);
+      card.appendChild(cardImg);
+      card.appendChild(cardBody);
+      
       document.getElementById("recipes").appendChild(card);
     }
   }
